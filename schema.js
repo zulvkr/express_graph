478c0519f58d25bfe59f9371cf2e93e17f25c46c
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-05 15:56:02 +0700

    what ever

---
+++ schema.js
@@ -0,0 +1,91 @@
+const graphql = require('graphql')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} = graphql
+
+const posts = [
+  {
+    // id: 1,
+    title: 'First post',
+    description: 'Content of the first post',
+    author: 'Flavio'
+  },
+  {
+    // id: 2,
+    title: 'Second post',
+    description: 'Content of the second post',
+    author: 'Roger'
+  }
+]
+
+const authors = {
+  Flavio: {
+    name: 'Flavio',
+    age: 36
+  },
+  Roger: {
+    name: 'Roger',
+    age: 7
+  }
+}
+
+const authorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: {
+    name: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    }
+  }
+})
+
+const postType = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    title: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    },
+    author: {
+      type: authorType,
+      resolve: (source, params) => {
+        return authors[source.author]
+      }
+    }
+  }
+})
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    post: {
+      type: postType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (source, { id }) => {
+        return posts[id]
+      }
+    },
+    posts: {
+      type: new GraphQLList(postType),
+      resolve: () => {
+        return posts
+      }
+    }
+  }
+})
+
+const schema = new GraphQLSchema({
+  query: queryType
+})
+
+module.exports = schema
